05bc7373a1963441bd6f75c28b4e5cbb9b9d6a8c
Isaias Batista Franca <isaiasbatista96@gmail.com>
2019-12-15 19:55:57 -0300

    Project creation/sidebar creation and styling

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import { Container, NewPlaylist, Nav } from "./styles";
+
+import AddPlayListIcon from "../../assets/images/add_playlist.svg";
+
+const Sidebar = () => (
+  <Container>
+    <div>
+      <Nav main>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Radio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>SUA BIBLIOTECA</span>
+        </li>
+        <li>
+          <a href="">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Tocados Recentemente</a>
+        </li>
+        <li>
+          <a href="">Músicas</a>
+        </li>
+        <li>
+          <a href="">Álbuns</a>
+        </li>
+        <li>
+          <a href="">Artistas</a>
+        </li>
+        <li>
+          <a href="">Estações</a>
+        </li>
+        <li>
+          <a href="">Arquivos Locais</a>
+        </li>
+        <li>
+          <a href="">Videos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>PLAYLISTS</span>
+        </li>
+        <li>
+          <a href="">Pagode 2019</a>
+        </li>
+      </Nav>
+    </div>
+    <NewPlaylist>
+      <img src={AddPlayListIcon} alt="Adicionar Playlist" />
+      Nova Playlist
+    </NewPlaylist>
+  </Container>
+);
+
+export default Sidebar;
